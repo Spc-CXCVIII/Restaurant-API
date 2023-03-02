75a75316df4652f6022aaf81afb210643a6a31dc
Supachai-198 <supachai.pho@outlook.com>
2023-03-02 16:48:37 +0700

    Update RestaurantController.php
    - Check received data but reasult = [] of 2 google APIs

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -7,59 +7,89 @@ use Illuminate\Support\Facades\Http;
 
 class RestaurantController extends Controller
 {
-    public function __construct()
-    {
-        date_default_timezone_set('Asia/Bangkok');
-        header('Content-Type: application/json');
-    }
+  public function __construct()
+  {
+    date_default_timezone_set('Asia/Bangkok');
+    header('Content-Type: application/json');
+  }
 
-    public function SearchByKeyword(Request $request)
-    {
-      try
-      {
-        $credentials_data = $request->validate([
-          'keyword' => 'nullable|required|string'
-        ]);
+  public function SearchByKeyword(Request $request)
+  {
+    try {
+      $credentials_data = $request->validate([
+        'keyword' => 'nullable|required|string'
+      ]);
 
-        //! Get lat, lon from Keyword
-        $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json', [
-            'address' => $credentials_data['keyword'],
-            'key' =>     env('GOOGLE_MAP_API_KEY'),
-        ]);
-        
-        $results = $response->json();
-        $lat = $results['results'][0]['geometry']['location']['lat'];
-        $lng = $results['results'][0]['geometry']['location']['lng'];
+      //! Get lat, lon from Keyword
+      $response = Http::get('https://maps.googleapis.com/maps/api/geocode/json', [
+        'address' => $credentials_data['keyword'],
+        'key' =>     env('GOOGLE_MAP_API_KEY'),
+      ]);
 
-        //! Find the restaurant
+      //! Check if keyword is valid
+      if (count($response->json()['results']) === 0) {
+        return response(
+          array(
+            'error_description' => 'Keyword not found',
+            'status' => 'error'
+          ),
+          200
+        );
+      }
 
-        $restaurant_list = [];
+      //! Get lat, lon from Keyword
+      $results = $response->json();
+      $lat = $results['results'][0]['geometry']['location']['lat'];
+      $lng = $results['results'][0]['geometry']['location']['lng'];
+
+      //! Find the restaurant
+      $restaurant_list = [];
+      //! First query
+      $response = Http::get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', [
+        'location' => $lat . ', ' . $lng,
+        'radius' => 1500,
+        'type' => 'restaurant',
+        'key' => env('GOOGLE_MAP_API_KEY'),
+      ]);
+
+      //! Add $response['results'] to $restaurant_list
+      $restaurant_list = array_merge($restaurant_list, $response['results']);
+      //! Query next page if data $response has next_page_token
+      while (isset($response['next_page_token'])) {
+        sleep(1);
         $response = Http::get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', [
-            'location' => $lat . ', ' . $lng,
-            'radius' => 100,
-            'type' => 'restaurant',
-            'key' => env('GOOGLE_MAP_API_KEY'),
+          'pagetoken' => $response['next_page_token'],
+          'key' => env('GOOGLE_MAP_API_KEY'),
         ]);
-        
         $restaurant_list = array_merge($restaurant_list, $response['results']);
-        while (isset($response['next_page_token'])) {
-            sleep(1);
-            $response = Http::get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', [
-                'pagetoken' => $response['next_page_token'],
-                'key' => env('GOOGLE_MAP_API_KEY'),
-            ]);
-            $restaurant_list = array_merge($restaurant_list, $response['results']);
-        }
-        
-        return $restaurant_list;
       }
 
-      catch (\Exception $e)
-      {
-        return response()->json([
-          'status' => 'error',
-          'message' => $e->getMessage()
-        ], 500);
+      if (count($restaurant_list) === 0) {
+        return response(
+          array(
+            'error_description' => 'Keyword not found',
+            'status' => 'error'
+          ),
+          200
+        );
       }
+
+      //! Return the result
+      return response(
+        array(
+          'results' => $restaurant_list,
+          'status' => 'ok'
+        ),
+        200
+      );
+    } catch (\Exception $e) {
+      return response(
+        array(
+          'error_description' => $e->getMessage(),
+          'status' => 'error'
+        ),
+        200
+      );
     }
+  }
 }
